245d6061c2ecacf0a5aa3ad092c5de93b52b5e08
Lloyd van Zaalen <lfb0801@github.com>
2020-05-15 23:34:51 +0200

    minor changes

--- paddleshiftercontrol/paddleshiftercontrol.ino
+++ paddleshiftercontrol/paddleshiftercontrol.ino
@@ -1,5 +1,5 @@
-#define leftPaddlePin 0
-#define rightPaddlePin 1
+#define leftPaddlePin 11
+#define rightPaddlePin 12
 
 char Gears[] = {'P', 'R', 'N', 'D', '2', '1'};
 int selectedGear = 0;
@@ -19,14 +19,17 @@ PaddleState currentPaddles;
 PaddleState lastPaddles;
 Timer timerDisplay;
 
-void setup (void) {
+void setup() {
   TimerStart ( & timerDisplay );
   timerDisplay = { 0, 3000 };
+
+  pinMode(leftPaddlePin, INPUT);
+  pinMode(rightPaddlePin, INPUT);
 
   Serial.begin(9600);
 }
 
-void loop (void) {
+void loop() {
   //  Read shifters
   currentPaddles = { digitalRead(leftPaddlePin), digitalRead(rightPaddlePin) };
 
@@ -46,7 +49,7 @@ void loop (void) {
 
 //  Display Code
 
-void taskDisplay ( void ) {
+void taskDisplay() {
   //  Display Gears[selectedGear]
 }
 
@@ -62,12 +65,11 @@ bool comparePaddles(struct PaddleState * p0, struct PaddleState * p1) {
   }
 }
 
-void shift ( struct PaddleState * paddles ) {
+void shift(struct PaddleState * paddles) {
   if (paddles->right && paddles->left) {
-    //  Both paddles pressed
-    //  Maybe special action
   }
   if (!paddles->right && paddles->left) {
+    //  if (allowed to switch)  //  based on speed sensor
     Serial.println("Shifted up");
   }
   if (paddles->right && !paddles->left) {
@@ -77,12 +79,12 @@ void shift ( struct PaddleState * paddles ) {
 
 //  Timer Code
 
-char TimerExpired ( struct Timer * timer ) {
+char TimerExpired(struct Timer * timer) {
   if ( millis () > timer->start + timer->timeout )
     return true;
   return false;
 }
 
-void TimerStart ( struct Timer * timer ) {
+void TimerStart(struct Timer * timer) {
   timer->start = millis ( );
 }
